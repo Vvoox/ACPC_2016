42915686f2d7eb3f8080c7ccbaa4308cf00b1a5c
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2019-04-17 09:19:18 +0100

    Add files via upload

--- Valar_Morghulis/src/com/example/java/Main.java
+++ Valar_Morghulis/src/com/example/java/Main.java
@@ -1,30 +1,33 @@
 package com.example.java;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
+import java.util.*;
 
 public class Main {
 
     public static int N = 0, E = 0, Q = 0;
 
     public static void main(String[] args) {
-        System.out.println("We need to help Khaleesi to split the army into 2 sides : ");
         System.out.println("Insert your input : ");
         Scanner cs = new Scanner(System.in);
         int T = cs.nextInt();
         Scanner num = new Scanner(System.in);
         Scanner riv = new Scanner(System.in);
         Scanner que = new Scanner(System.in);
-
-
+        String input[] = new String[3];
+        int input1[] = new int[3];
         for (int i = 0; i < T; i++) {
-            String nn = num.next();
-            String ee = riv.next();
-            String qq = que.next();
-            N = Integer.parseInt(nn);
-            E = Integer.parseInt(ee);
-            Q = Integer.parseInt(qq);
-            valar(N ,E, Q);
+            input[0] = num.next();
+            input[1] = riv.next();
+            input[2] = que.next();
+            input1[0] = Integer.parseInt(input[0]);
+            input1[1] = Integer.parseInt(input[1]);
+            input1[2] = Integer.parseInt(input[2]);
+
+            valar(input1[0] ,input1[1], input1[2]);
         }
+
     }
 
     public static void valar(int N ,int E, int Q) {
@@ -54,21 +57,6 @@ public class Main {
             }
         }
 
-        /*for (int i = 0; i < E; i++) {
-            for (int j = 0; j < 2; j++) {
-
-                System.out.print(rivalries[i][j] + " ");
-            }
-            System.out.println();
-        }
-       /* for (int i = 0; i < Q; i++) {
-            for (int j = 0; j < 2; j++) {
-
-                System.out.print(queries[i][j] + " ");
-            }
-            System.out.println();
-        }*/
-
         for (int i = 0; i < Q; i++) {
 
             int length = queries[i][1] - queries[i][0] + 1 ;
@@ -82,7 +70,7 @@ public class Main {
             }
 
             for (int j = 0; j < length; j++) {
-                //System.out.println(i);
+
 
                 if (rivalries[i][0] == interval[j]) {
 
@@ -122,20 +110,6 @@ public class Main {
                         else break;
                     }
                 }
-                /*if (rivalries[i][1] != interval[j] && rivalries[i][0] != interval[j]) {
-
-                    for (int k =0; k < length; k++) {
-
-                        if (rivalries[i][0] != interval[k] && rivalries[i][0] != interval[k]) {
-
-                            resultat[i][0] = N/2;
-                            resultat[i][1] = N/2;
-
-                        }
-
-                        else break;
-                    }
-                }*/
                 if(length==1){
 
                     if(rivalries[i][0]==interval[j]){
@@ -152,22 +126,6 @@ public class Main {
                 }
 
 
-
-                /*if(rivalries[i][0]!=interval[j]) {
-
-                    for(int y =0 ; y<length ; y++){
-
-                        if(rivalries[i][1]!=interval[y]){
-
-                            resultat[i][0] = 5;
-                            resultat[i][1] = 5;
-                        }
-                        else break;
-                    }
-
-
-
-                }*/
             }
 
             }
@@ -179,4 +137,5 @@ public class Main {
         }
 
     }
+
 }
